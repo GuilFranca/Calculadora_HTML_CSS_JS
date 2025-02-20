d899acaf4085bbed40394db6c8118843a7b6a6a1
GuilFranca <guilfranca27@gmail.com>
2025-02-20 18:26:39 -0300

    Implementando as funcionalidades de calcular e limpar pelo teclado

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 
-const limpar = document.querySelector(".limpar");
+const limparClick = document.querySelector(".limpar");
 
 const apagar = document.querySelector(".apagar");
 
-const calcular = document.querySelector(".calcular");
+const calcularClick = document.querySelector(".calcular");
 
 let resultado = document.getElementById("resultado");
 
@@ -17,8 +17,19 @@ botoes.forEach(botao => {
     })
 })
 
-limpar.addEventListener("click", function limpar() {
+function limpar() {
     resultado.value = '';
+}
+
+limparClick.addEventListener("click", function(){
+    limpar();
+})
+
+resultado.addEventListener("keydown", function(event) {
+    if (event.key === "c") {
+        limpar();
+        event.preventDefault();
+    }
 })
 
 apagar.addEventListener("click", function apagar() {
@@ -26,8 +37,19 @@ apagar.addEventListener("click", function apagar() {
     resultado.value = resultado.value.slice(0, -1);
 })
 
-calcular.addEventListener("click", function calcular() {
+function calcular() {
     if(resultado.value) {
         resultado.value = eval(resultado.value);
     }
+}
+
+calcularClick.addEventListener("click", function() {
+    calcular();
 })
+
+resultado.addEventListener("keydown", function(event) {
+    if(event.key === "Enter") {
+        calcular();
+    }
+})
+
